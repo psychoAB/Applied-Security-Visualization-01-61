6e5b5e27c82d3b4cb159c83c3ca3998ff49aa268
psychoAB <natchanan.th@ku.th>
2018-08-31 09:05:59 +0700

    week02: added ip_usage_ratio

---
+++ 02/src/ip_usage_ratio.js
@@ -0,0 +1,48 @@
+var dom = document.getElementById("graph");
+var chart = echarts.init(dom);
+
+chart.showLoading();
+
+
+$.getJSON("src/ip_usage_ratio.json", function(json) {
+    chart.hideLoading();
+
+    chart.setOption(option = {
+        title : {
+            text: 'IP usage ratio',
+            x:'center'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: json.map(function (element) {
+                return {
+                    name: element.Protocol
+                };
+            })
+        },
+        series : [
+            {
+                name: 'IP usage ratio',
+                type: 'pie',
+                radius : '55%',
+                center: ['50%', '40%'],
+                data: json.map(function (element) {
+                    return {
+                        name: element.Protocol,
+                        value: element.Usage
+                    };
+                }),
+                label: {
+                    normal: {
+                        formatter: '{b}\n{d}%'
+                    }
+                }
+            }
+        ]
+    }, true);
+});
