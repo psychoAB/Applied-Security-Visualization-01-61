3300115c762c60322d522008241e503ba35b2bc9
psychoAB <natchanan.th@ku.th>
2018-09-07 13:42:17 +0700

    week04: completed 3.4

---
+++ 04/src/script/ingress_hostname_distribution.js
@@ -0,0 +1,30 @@
+var dom = document.getElementById("graph");
+var chart = echarts.init(dom);
+
+chart.showLoading();
+
+$.getJSON("src/json/ingress_hostname_distribution.json", function(json) {
+    chart.hideLoading();
+
+    chart.setOption(option = {
+        title : {
+            text: '3.4 Ingress hostname distribution',
+            x:'center'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{b}<br/>Requests : {c}"
+        },
+        series: [
+            {
+                type: 'treemap',
+                data: json.map(function (record) {
+                    return {
+                        name: record.hostname,
+                        value: record.count
+                    };
+                }),
+            }
+        ]
+    }, true);
+});
