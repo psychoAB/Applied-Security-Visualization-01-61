5589c6dcc61c113ec38580d66850b81aa9b8657a
psychoAB <natchanan.th@ku.th>
2018-09-07 09:19:46 +0700

    week04: completed 3.1.2

--- 04/src/script/users_requests_percentage.js
+++ 04/src/script/users_requests_percentage.js
@@ -1,1 +1,31 @@
-console.log('testing');
+var dom = document.getElementById("graph");
+var chart = echarts.init(dom);
+
+chart.showLoading();
+
+
+$.getJSON("src/json/users_requests_percentage.json", function(json) {
+    chart.hideLoading();
+
+    chart.setOption(option = {
+        title : {
+            text: '3.1.2 Requests per minute',
+            x:'center'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{b}<br/>Requests : {c}"
+        },
+        series: [
+            {
+                type: 'treemap',
+                data: json.map(function (record) {
+                    return {
+                        name: record.user,
+                        value: record.count
+                    };
+                }),
+            }
+        ]
+    }, true);
+});
